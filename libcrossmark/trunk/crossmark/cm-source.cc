baa2e19b9f5093b3aa6890749349da302f7920c4
robsta <robsta@bcba8976-2d24-0410-9c9c-aab3bd5fdfd6>
2006-12-13 14:53:38 +0000

    parser work

--- libcrossmark/trunk/crossmark/cm-source.cc
+++ libcrossmark/trunk/crossmark/cm-source.cc
@@ -42,8 +42,88 @@ Source::~Source ()
 	delete &_scanner;
 }
 
-bool 
+/*
+# Scanner tokens:
+sof eof pbreak newline indent text 
+style-lb style-rb style-li style-ri style-lm style-rm style-lu style-ru
+
+# Some quickie top-down grammar, flaws:
+# + way incomplete
+# + arbitrary nesting of styling allowed
+# + ...
+
+document   := sof newline* ( pbreak* (paragraph | blockquote) )* pbreak* eof
+paragraph  := ( text | markup )* pbreak
+blockquote := indent line ( newline indent line )* pbreak
+line       := ( text | markup )*
+markup     := bold | italic | monospace | underline
+bold       := style-lb ( text | markup )* style-rb
+italic     := style-li ( text | markup )* style-ri
+monospace  := style-lm ( text | markup )* style-rm
+underline  := style-lu ( text | markup )* style-ru
+*/
+gboolean 
 Source::sputter ()
+{
+	parseDocument ();
+}
+
+void 
+Source::parseDocument ()
+{
+	_reader.pushDocument ();
+
+	// fetch token
+
+	_reader.popDocument ();
+}
+
+void 
+Source::parseParagraph ()
 {
 
 }
+
+void 
+Source::parseBlockquote ()
+{
+
+}
+
+void 
+Source::parseLine ()
+{
+
+}
+
+void 
+Source::parseMarkup ()
+{
+
+}
+
+void 
+Source::parseBold ()
+{
+
+}
+
+void 
+Source::parseItalic ()
+{
+
+}
+
+void 
+Source::parseMonospace ()
+{
+
+}
+
+void 
+Source::parseUnderline ()
+{
+
+}
+
+
