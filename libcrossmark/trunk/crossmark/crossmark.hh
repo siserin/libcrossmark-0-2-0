268e6797f63aaddd2b48bd7f9b565a1a9d3f49f6
robsta <robsta@bcba8976-2d24-0410-9c9c-aab3bd5fdfd6>
2006-12-20 14:40:09 +0000

    more refactoring

--- libcrossmark/trunk/crossmark/crossmark.hh
+++ libcrossmark/trunk/crossmark/crossmark.hh
@@ -25,11 +25,9 @@
 #ifndef CROSSMARK_HH
 #define CROSSMARK_HH
 
-#include <stack>
-#include <string>
 #include <crossmark/cm-features.hh>
-#include <crossmark/cm-document.hh>
-#include <crossmark/cm-stream.hh>
+#include <crossmark/cm-sink.hh>
+#include <crossmark/cm-source.hh>
 
 /*!
  * \brief Toplevel crossmark namespace. 
@@ -45,83 +43,7 @@
  */
 namespace crossmark {
 
-namespace tokens {
-	class Token;
-};
-class Scanner;
-class Validator;
-
-/*!
- * \brief Crossmark parser implementation.
- */
-class Source 
-{
-public:
-	Source (const std::string &file, 
-		Document &reader);
-
-	Source (stream::Input &istream, 
-		Document &reader);
-
-	virtual ~Source ();
-
-	/*!
-	 * \todo use GError or exceptions?
-	 */
-	gboolean sputter ();
-
-protected:
-	void parseDocument ();
-	tokens::Token * parseParagraph (const tokens::Token *first);
-	tokens::Token * parseBlockquote (const tokens::Token *first);
-	tokens::Token * parseLine (const tokens::Token *first);
-	tokens::Token * parseMarkup (const tokens::Token *first);
-	tokens::Token * parseStyle (const tokens::Token *first, document::Style::Type type);
-	tokens::Token * parseText (const tokens::Token *first);
-
-private:
-	Scanner   *_scanner;
-	Validator *_validator;
-};
-
-/*!
- * \brief Writer must implement all specific document interfaces.
- *
- * \note Maybe later we'll split mandatory and optional ones.
- *
- * \todo Does the writer validate?
- * \todo Implement state machine.
- */
-class Writer : public Document
-{
-friend class Sink;
-public:
-	virtual ~Writer ();
-
-	virtual void text (const std::string &text);
-
-	virtual void pushStyle (document::Style::Type type);
-	virtual void popStyle (document::Style::Type type);
-
-	// TODO implement
-
-private:
-	void setStream (FILE *ostream);
-
-	FILE *_ostream;
-};
-
-/*!
- * \brief Wrapper for opening and closing files or streams to write to.
- */
-class Sink
-{
-public:
-	Sink (const std::string &file, 
-	      Writer &writer);
-
-	virtual ~Sink ();
-};
+/* placeholder for docs */
 
 };  // namespace crossmark
 
