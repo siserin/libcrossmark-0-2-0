2187d7122e221602a0b1424a64b3bf0f70d42fa8
robsta <robsta@bcba8976-2d24-0410-9c9c-aab3bd5fdfd6>
2006-12-20 14:59:47 +0000

    utf8 for std stream

--- libcrossmark/trunk/crossmark/cm-gsf-stream.cc
+++ libcrossmark/trunk/crossmark/cm-gsf-stream.cc
@@ -20,45 +20,9 @@
 #include <gsf/gsf-input-stdio.h>
 #include <gsf/gsf-output-stdio.h>
 #include "cm-gsf-stream-private.hh"
+#include "cm-stream-private.hh"
 
 using namespace crossmark;
-
-// Stolen from glib's gutf8.c
-#define UTF8_COMPUTE(Char, Mask, Len)					      \
-  if (Char < 128)							      \
-    {									      \
-      Len = 1;								      \
-      Mask = 0x7f;							      \
-    }									      \
-  else if ((Char & 0xe0) == 0xc0)					      \
-    {									      \
-      Len = 2;								      \
-      Mask = 0x1f;							      \
-    }									      \
-  else if ((Char & 0xf0) == 0xe0)					      \
-    {									      \
-      Len = 3;								      \
-      Mask = 0x0f;							      \
-    }									      \
-  else if ((Char & 0xf8) == 0xf0)					      \
-    {									      \
-      Len = 4;								      \
-      Mask = 0x07;							      \
-    }									      \
-  else if ((Char & 0xfc) == 0xf8)					      \
-    {									      \
-      Len = 5;								      \
-      Mask = 0x03;							      \
-    }									      \
-  else if ((Char & 0xfe) == 0xfc)					      \
-    {									      \
-      Len = 6;								      \
-      Mask = 0x01;							      \
-    }									      \
-  else									      \
-    Len = -1;
-
-
 
 /*!
  * Create input from a GsfInput.
@@ -120,7 +84,6 @@ gunichar
 stream::GsfInput::getChar ()
 {
 	guint8        buf[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
-	guint8 const *iter;
 	int 	      mask;
 	int           len;
 
@@ -130,12 +93,12 @@ stream::GsfInput::getChar ()
 		return (unsigned) EOF;
 	}
 
-	iter = gsf_input_read (_input, 1, buf);
+	gsf_input_read (_input, 1, buf);
 	mask = 0;
-	UTF8_COMPUTE (*iter, mask, len);
+	UTF8_COMPUTE (buf[0], mask, len);
 	if (len > 1) {
 		// read remaining bytes
-		iter = gsf_input_read (_input, len - 1, buf + 1);
+		gsf_input_read (_input, len - 1, buf + 1);
 	}
 	
 	return g_utf8_get_char ((const gchar *) buf);
